1d5b1729c83707d1d14b4548489e9300a9658f4a
unknown <>
2016-07-10 17:00:07 +0800

    绘制三角形

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,18 @@
-window.onload(function () {
+window.onload = function () {
     var canvas = document.getElementById('canvas');
     // 调用canvas 2d接口
     var context = canvas.getContext('2d');
+    // 绘制三角形
+    context.moveTo(100,100);
+    context.lineTo(700,700);
+    context.lineTo(700,100);
+    context.lineTo(100,100);
+    // 样式
+    context.lineWith = 10;
+    context.strokeStyle = '#058';
+    // 最后描边
+    context.stroke();
+
 
     
-})
+};
