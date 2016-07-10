91297ad1c6d1b7bef67161176f4cb9423cf0c3c8
unknown <>
2016-07-10 22:06:06 +0800

    绘制圆形

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,21 +2,15 @@ window.onload = function () {
     var canvas = document.getElementById('canvas');
     // 调用canvas 2d接口
     var context = canvas.getContext('2d');
-    // 绘制三角形
-    context.moveTo(100,100);
-    context.lineTo(700,700);
-    context.lineTo(700,100);
-    context.lineTo(100,100);
-    // 样式
-    // 最后描边
+    // 绘制圆形 样式
+    context.lineWidth = 5;
+    context.strokeStyle = '#058';
+    // 绘制圆形的形状
 
-
-    context.fillStyle = 'rgb(2,100,30)';
-    context.fill();
-
-    context.lineWith = 20;
-    context.strokeStyle = 'red';
+    context.arc(300,300,200,0,1.5*Math.PI);
     context.stroke();
+
+
 
     
 };
