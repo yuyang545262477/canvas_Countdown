b658d2c74e49690b49dc64a5dabe42e60e293cdb
unknown <>
2016-07-10 16:50:10 +0800

    初始化canvas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+window.onload(function () {
+    var canvas = document.getElementById('canvas');
+    // 调用canvas 2d接口
+    var context = canvas.getContext('2d');
+
+    
+})
